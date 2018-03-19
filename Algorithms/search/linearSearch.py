50401a0f836140defc7a4d9e9083a90619647153
Puneet Verma <puneetverm24@gmail.com>
2018-03-19 13:56:11 +0530

    Added linear Search

---
+++ Algorithms/search/linearSearch.py
@@ -0,0 +1,15 @@
+
+def linearSearch(arr):
+      for i in range(len(arr)):
+          if arr[i] == x:
+           return "Found Element"
+
+      return "Not Found"
+
+arr=[2,6,8,3,6,56,78]
+x=6
+print(linearSearch(arr))
+
+
+
+
